c63770eb611cc60ae75fb35066344ebad8aed019
Neworld <neworld@mole.land>
2020-08-28 08:11:24 +0900

    [UPDATE] update base_model_test.dart

--- test/core/viewmodels/base_model_test.dart
+++ test/core/viewmodels/base_model_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_provider_example_for_blog/core/enums/viewmodel_state.dart';
 import 'package:flutter_provider_example_for_blog/core/viewmodels/base_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -13,9 +14,11 @@ void main() {
 
   test('BaseModel에서 setState의 동작 여부 테스트', () {
     // arrange
-
+    final state = ViewModelState.Idle;
     // act
-
+    baseModel.setState(ViewModelState.Idle);
+    final result = baseModel.state;
     // assert
+    expect(state, result);
   });
 }
