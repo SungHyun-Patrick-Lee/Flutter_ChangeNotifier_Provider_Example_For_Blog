f2221f158d511d002bdb97e323f6bb77355627f6
Neworld <neworld@mole.land>
2020-09-03 08:31:22 +0900

    [ADD] injection_container.dart

---
+++ lib/injection_container.dart
@@ -0,0 +1,32 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter_provider_example_for_blog/core/network/network_info.dart';
+import 'package:flutter_provider_example_for_blog/core/services/api.dart';
+import 'package:flutter_provider_example_for_blog/core/services/number_trivia_service.dart';
+import 'package:flutter_provider_example_for_blog/core/viewmodels/number_trivia_model.dart';
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+
+GetIt locator = GetIt.instance;
+
+void setUpLocator() async {
+  // !ViewModel
+  locator
+      .registerFactory(() => NumberTriviaModel(numberTriviaService: locator()));
+
+  // !Services
+  locator
+      .registerLazySingleton<NumberTriviaService>(() => NumberTriviaServiceImpl(
+            api: locator(),
+            networkInfo: locator(),
+          ));
+  locator.registerLazySingleton<Api>(() => ApiImpl(httpClient: locator()));
+
+  // !Core
+  locator.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(
+        dataConnectionChecker: locator(),
+      ));
+
+  // !other
+  locator.registerLazySingleton(() => http.Client());
+  locator.registerLazySingleton(() => DataConnectionChecker());
+}
