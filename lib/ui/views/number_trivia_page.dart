2833fe616c9c006b1b524e8e4a75f38cc2ba8f5b
Neworld <neworld@mole.land>
2020-09-04 08:06:27 +0900

    [UPDATE] build first ui

--- lib/ui/views/number_trivia_page.dart
+++ lib/ui/views/number_trivia_page.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_provider_example_for_blog/core/viewmodels/number_trivia_model.dart';
+import 'package:flutter_provider_example_for_blog/injection_container.dart';
+import 'package:provider/provider.dart';
 
 class NumberTriviaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('NumbetTriviaRandom'),
+        title: Text('NumberTriviaRandom'),
+      ),
+      body: SingleChildScrollView(
+        child: buildBody(context),
+      ),
+    );
+  }
+
+  ChangeNotifierProvider<NumberTriviaModel> buildBody(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => locator<NumberTriviaModel>(),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            child: Placeholder(),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ],
       ),
     );
   }
